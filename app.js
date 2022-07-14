71cb69022af18f7808b27f0e7478fd8d403a456e
RAMAVATH JAYA RAM NAIK <76204860+ZeroOp@users.noreply.github.com>
2022-07-14 15:07:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const DataBase = require('./database')
 const path = require('path')
 const app = express();
 
-const port = process.env.PORT 3000;
+const port = process.env.PORT || 3000;
 
 app.use(session({
     secret:"Hello World",
